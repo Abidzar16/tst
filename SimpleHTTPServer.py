427fcda4b16609a834436a0b73be9fdcebc72996
Abidzar16 <abidzarisprivate@gmail.com>
2019-09-30 21:01:07 +0700

    Update and rename simplehttpserver.py to SimpleHTTPServer.py

--- SimpleHTTPServer.py
+++ SimpleHTTPServer.py
@@ -21,10 +21,3 @@ handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", port), handler) as httpd:
     print("serving at port", port)
     httpd.serve_forever()
-
-
-# In[ ]:
-
-
-
-
